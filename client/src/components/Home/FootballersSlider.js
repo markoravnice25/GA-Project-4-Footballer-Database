0c31a7a99ddb943151908724389349d426b3948c
Marko Filipovic <markoravnice25@gmail.com>
2022-07-01 18:30:14 +0200

    changed all images in FootballSlider.js to cloudinary files rather than accessed from public folder

--- client/src/components/Home/FootballersSlider.js
+++ client/src/components/Home/FootballersSlider.js
@@ -18,11 +18,11 @@ const FootballersSlider = () => {
   return (
     <Slider {...settingsSingle}>
       <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692327/project-4-footballer-database/Argentina_kuqujs.jpg" alt="Argentina" />
-      <img src="/images/Qatar.jpeg" alt="Qatar" />
-      <img src="/images/USA.jpg" alt="USA" />
-      <img src="/images/Australia.jpeg" alt="Australia" />
-      <img src="/images/Italy.webp" alt="Italy" />
-      <img src="/images/Senegal.jpeg" alt='Senegal' />
+      <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692317/project-4-footballer-database/Qatar_vyqzfb.jpg" alt="Qatar" />
+      <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692308/project-4-footballer-database/USA_ksbavl.jpg" alt="USA" />
+      <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692324/project-4-footballer-database/Australia_k5tg8l.jpg" alt="Australia" />
+      <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692320/project-4-footballer-database/Italy_aka2ir.webp" alt="Italy" />
+      <img src="https://res.cloudinary.com/dnj5og0ry/image/upload/v1656692314/project-4-footballer-database/Senegal_xmz0pa.jpg" alt='Senegal' />
     </Slider>
   )
 }
